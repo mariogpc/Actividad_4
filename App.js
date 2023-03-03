9c98d0ce4ecebc2a8a4669b2808c0acc40768911
mariogpc <mprado386@accitesz.com>
2023-03-02 23:53:04 -0600

    Actividad_4

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View, ts } from 'react-native';
 
 export default function App() {
+
+  const [materia1, setPrimerMateria] = useState("");
+  const [materia2, setSegundaMateria] = useState("");
+  const [materia3, setTercerMateria] = useState("");
+  const [texto, setTexto] = useState("");
+
+  const accion  = () => {
+    const text = materia1 + materia2 + materia3;
+    setTexto(text);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.cajaTexto}
+        value={materia1}
+        onChangeText={setPrimerMateria}
+      />
+      <TextInput
+        style={styles.cajaTexto}
+        value={materia2}
+        onChangeText={setSegundaMateria}
+      />
+      <TextInput
+        style={styles.cajaTexto}
+        value={materia3}
+        onChangeText={setTercerMateria}
+      />
+      <Button
+        title="Mostrar texto"
+        onPress={accion}
+      />
+      <Text>{texto}</Text>
     </View>
   );
 }
@@ -17,4 +46,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  cajaTexto:{
+    borderColor: "black",
+    borderWidth: 2,
+    Width: 80,
+  },
+})
